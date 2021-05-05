53b98e4de819e287cc451f689861aeb76c841a42
ChillinAkaMac <44977674+Macintosh1983@users.noreply.github.com>
2021-05-04 20:22:55 -0500

    Update mainESP.lua

--- mainESP.lua
+++ mainESP.lua
@@ -22,6 +22,7 @@ function findpig()
 
 local imposter = findsussy()
 local pig = findpig()
+local ME = game.Players.LocalPlayer
 
 function imposterESP()--Gives Murderer an unfair advantage LOOOL
     for i,v in pairs(game.Players:GetPlayers()) do
@@ -45,13 +46,14 @@ function imposterESP()--Gives Murderer an unfair advantage LOOOL
 end
 
 function esp()
-    if v.Name == imposter then
+    for i,v in pairs(game.Players:GetPlayers()) do
+        if v.Name == imposter then
             for i, v in pairs(v.Character:GetDescendants()) do
                 if v:IsA("Part") or v:IsA("MeshPart") then
                     if v.Name == "BoxHandleAdornment" then
                         v:Destroy()
                     end
-                    local funny = Instance.new("BoxHandleAdornment")--MURDERER ESP
+                    local funny = Instance.new("BoxHandleAdornment")
                     funny.Parent = v
                     funny.Adornee = v
                     funny.AlwaysOnTop = true
@@ -59,7 +61,6 @@ function esp()
                     funny.ZIndex = 0
                     funny.Size = v.size
                     funny.Color3 = Color3.fromRGB(255, 0, 4)
-                    imposterESP()
                 end
             end
         elseif v.Name == pig then
@@ -68,7 +69,7 @@ function esp()
                         if v.Name == "BoxHandleAdornment" then
                             v:Destroy()
                         end
-                        local funny = Instance.new("BoxHandleAdornment")--SHERIFF ESP
+                        local funny = Instance.new("BoxHandleAdornment")
                         funny.Parent = v
                         funny.Adornee = v
                         funny.AlwaysOnTop = true
@@ -81,27 +82,19 @@ function esp()
             end
         end
     end
-
+end
 Notify({Title="MM2 AUTO ESP LOADED!";Text="ENJOY KILLING NUBS"})
 wait(1)
 Notify({Title="DISCORD INV COPIED";Text="JOIN THE DISCORD FOR UPDATES"})
 setclipboard("discord.gg/8gt6mqr")
 
-local mt = getrawmetatable(game)
-setreadonly(mt, false)
-local namecall = mt.__namecall
-mt.__namecall = newcclosure(function(self, ...)
-local method = getnamecallmethod()
-local args = {...}
-if tostring(method) == "InvokeServer" and tostring(self) == "GetChance" then
-spawn(function()
-wait(15)
+
 if imposter and pig then
     Notify({Title="The Murderer is";Text=imposter;Icon="http://www.roblox.com/Thumbs/Avatar.ashx?x=150&y=150&Format=Png&username="..imposter})
     Notify({Title="The Sheriff is";Text=pig;Icon="http://www.roblox.com/Thumbs/Avatar.ashx?x=150&y=150&Format=Png&username="..pig})
     esp()
-            end
-        end)
-    end
-return namecall(self, table.unpack(args))
-end)
+end
+
+if imposter == ME then
+    imposterESP()
+end
